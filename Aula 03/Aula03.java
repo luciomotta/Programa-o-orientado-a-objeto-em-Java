931b51103a5ce73a5822ffc78f3fbe62292fd72d
luciomotta <83682095+luciomotta@users.noreply.github.com>
2023-03-16 21:23:19 -0300

    Add files via upload

---
+++ Aula 03/Aula03.java
@@ -0,0 +1,14 @@
+class Aula03    //(Conta bancaeria)
+{
+    private String nome;    //Variavel de instancia ou atributo
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;   // Armazenar o nome (Com metodo construtor THIS)
+        
+    }
+    public String getNome() //Metodo para recuperar o nome
+    {
+        return nome;    //Retornar o valor da chamada
+    }
+}   //Fim da classe Aula03
